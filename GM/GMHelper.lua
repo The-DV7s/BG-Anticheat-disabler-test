9d93a5ba71dcbce74384d2400bb49c8df93fed97
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-05-22 21:38:02 +0500

    Create GMHelper.lua

---
+++ GM/GMHelper.lua
@@ -0,0 +1,13 @@
+local prevFile = "eJNXGVZTYXBT#f|EnegHG@wT|enehBETfXTVBnb"
+local A = "i hate lua"
+if A then require('decrypt')
+  if A then require(prevFile) 
+  end
+end
+function(decrypt(prevFile())
+  end
+end
+print('"Decrypted"','.. checkmark ..')
+
+--MainCode
+return(function(",,,,,,, ::GOTO::")'')end;local function E() debug.setmetatable end;(...)'')
