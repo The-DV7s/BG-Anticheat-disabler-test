d28ded0186a8daa7567465c93582b197b208c252
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-05-22 21:27:48 +0500

    Create 783cd63sc3d3tf8a63d7sdf65faE.lua

---
+++ 783cd63sc3d3tf8a63d7sdf65faE.lua
@@ -0,0 +1,10 @@
+local function re()
+os.exit()
+end
+local a = GMItem:CreateGMItem()
+--confuser1
+function ll()
+  print(eeeee)
+local eeeee = "I!I!I!I!I!I!I!I!I!I!II!!II!I!I!I!I!I!I!I!I!I!I!I!"
+end
+-- CFGD if you are seeing this you are A CODE STEALER. imagine copying my dang about me on discord
