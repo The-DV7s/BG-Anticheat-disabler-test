8c3f160774612d4e6ecc024e3a778c041569cf2b
Gai | DV7 <167229955+xyzkyle@users.noreply.github.com>
2024-05-23 14:07:02 +0500

    Create errors.lua

---
+++ Handler/errors.lua
@@ -0,0 +1,10 @@
+--[[
+////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+-----------------------------------------------------////////
+--This file was obfuscated with DV7 Obfuscator-----////////
+-------------------------------------------------////////
+]]--
+
+local e=function()print('...')end;local func=[[ is.GetPlayerId(true ]];local f;local ed;if e then f="oOOOOooo0o0oo00oo0o00o0o0oo0o0o0o0o0o00o0o0o0oo0o0o0o0oo0o0Oo00o0o0o0O0o0OO0O00O0o0O0Oo00O0oO00o0Oo00o0O0o0Oo00Oo00Oo0oO0o0oo00o0o0oo000oo0o00o0o0o0oo0o0o0o0ooOooOoOoO"Game={};function Game:init()handler.getErrors()end return Game function lIlliLLIiLILIILLIILilil() print("Guys look") end print("Im imidating");function(eee0)end local eee0="print('welcome')" if eee0 then ed="NXT#7tx6xt87fn$TXYUWZDGNhnxetygxb#T^#^%VB%X#NHT#YNB$ICUUIH#YBGS#^&BVC^%#*&#NXCGFY4bg"local lIILLIllILILLILILILIILILIlillilililliilililiLILilililliLILILILiLilILililiILililILILLIILiLILILILILILiIILLIILLIILIL=function() print(ed) end return errors
